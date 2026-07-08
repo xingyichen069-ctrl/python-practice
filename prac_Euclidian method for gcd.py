44cba702120c47ff5bbf78b74f2803f8cf8d6f16
MotoroOkina <xingyichen069@gmail.com>
2026-07-08 08:16:48 +0800

    Add files via upload

--- prac_Euclidian method for gcd.py
+++ prac_Euclidian method for gcd.py
@@ -16,13 +16,4 @@ b=int(input("请输入数字二："))
 print(f"最大公约数gcd为：{gcd(a,b)}")
 print(f"最小公倍数lcm为：{int(a*b/gcd(a,b))}")
 
-#or:
-
-def k_fib(k):
-    a,b=1,1
-    for i in range(k-1):
-        a,b = b,a+b
-        return a
-
-n= int(input("Please input the position of your wanted number: "))
-print(f"Your wanted number is: {k_fib(n)}")
+#意义等级 ：1⭐
--- prac_Fibonacci sequence.py
+++ prac_Fibonacci sequence.py
@@ -7,3 +7,15 @@ def n_fib(n):
 n= int(input("Please input the position of your wanted number: "))
 print(f"Your wanted number is: {n_fib(n)}")
 
+#or:
+
+def k_fib(k):
+    a,b=1,1
+    for i in range(k-1):
+        a,b = b,a+b
+        return a
+
+n= int(input("Please input the position of your wanted number: "))
+print(f"Your wanted number is: {k_fib(n)}")
+
+#意义等级 ：1⭐
